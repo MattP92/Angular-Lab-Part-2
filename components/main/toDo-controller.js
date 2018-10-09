0233b3d40427a5ee40bb1f7d73fd6bd68a96c332
Matt Polk <mapolk7@yahoo.com>
2018-10-08 20:17:44 -0400

    Angular Lab Part 2

--- components/main/toDo-controller.js
+++ components/main/toDo-controller.js
@@ -18,5 +18,18 @@
             task:"Take out trash",
             completed: true
                 }];
+
+        $ctrl.addTask=function(){
+            $ctrl.toDoList.push({task: $ctrl.newChore, completed: false});
+        };
+        
+        $ctrl.removeTask=function(index){
+            $ctrl.toDoList.splice(index, 1);
+        };
+        
+        $ctrl.completeTask=function(index){
+            $ctrl.toDoList[index].completed = true; 
+        };
+        
 });
 };
